4ce752091f068a6f2b343a90197a6e69b80c2e66
razbim-noi-cumva-la-lumina <stefanbuciujr+gh2@gmail.com>
2026-09-19 17:00:45 +0300

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.java
@@ -0,0 +1,18 @@
+class StockSpanner {
+    private final Stack<int[]> s = new Stack<>();
+    
+    public int next(final int price) {
+        int span = 1;
+        while (!s.isEmpty() && s.peek()[0] <= price)
+            span += s.pop()[1];
+
+        s.push(new int[]{price, span});
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
